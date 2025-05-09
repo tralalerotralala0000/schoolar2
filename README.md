90845547b190fad6b8857a812c1d449fea8851c4
Camilo Stiven Criollo Vallejos <criolloc449@gmail.com>
2025-05-09 17:50:35 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# schoolar2
+# schoolar
+This is a Fullstack project witch: HTML, CSS, JS, PHP, POSTGRES, DOCKER, BOOTSTRAP, TAILWIND, NGROK
---
+++ src/list_users.php
@@ -0,0 +1,46 @@
+<?php
+include('../config/database.php');
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" align="center">
+        <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>E_mail</th>
+            <th>Status</th>
+            <td>...</td>
+        </tr>
+        <tr>
+            <th>Jaider</th>
+            <th>Gomez</th>
+            <th>Jaider@mail.coml</th>
+            <th>Active</th>
+            <td>
+                <img src="icons/usuario.png" width="15">
+                <img src="icons/delete.png" width="15">
+                <img src="icons/search.png" width="15">
+            </td>
+        </tr>
+
+
+
+        <?php
+        //Here Code
+        $sql="select"
+
+        ?>
+            
+
+    </table>
+    
+</body>
+</html>
